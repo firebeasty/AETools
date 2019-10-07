a39681b1044ec87f02992ea56c8dd777108d4a24
Unknown <keith.patrick.morgan@gmail.com>
2019-10-07 15:41:20 -0600

    Render basic button with labels

---
+++ extract/extract_scriptFile.jsx
@@ -0,0 +1,7 @@
+﻿var myWin = new Window('palette',  "Extract", undefined);
+    myWin.orientation = "row";
+    
+var groupOne = myWin.add("group",undefined, "Group1");
+    groupOne.add("button", undefined, "Extract EXR");
+    
+ myWin.show();
