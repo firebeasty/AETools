a36f4b10c036ea8c79f1c12a8c243c5631488b95
Unknown <keith.patrick.morgan@gmail.com>
2019-10-07 21:30:06 -0600

    Working Script Panel
    Script can now create a floating panel that generates an EXtractoR and Exposure effect on selected layers. Included text field to edit default gamma correction on exposure effect.

--- extract/extract_scriptFile.jsx
+++ extract/extract_scriptFile.jsx
@@ -1,7 +1,76 @@
-﻿var myWin = new Window('palette',  "Extract", undefined);
-    myWin.orientation = "row";
+﻿{
+function myScript(thisObj) {
+    function myScript_buildUI(thisObj){
+            var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Extract EXR", undefined, {resizeable:true});
+            
+                //resource list for UI buttons
+            res = "group{orientation:'column',alignment:['fill','fill'],\
+                        groupOne: Panel{text:'Enabled Effects',orientation:'row',alignment:['fill','top'],alignChildren:['left','top']minimumSize:[50,50],\
+                            exr: Checkbox{text:'EXtractoR'},\
+                            gamma: Checkbox{text:'Exposure'},\
+                            gamLabel: StaticText{text:'Adjust Gamma:'},\
+                            gamEdit: EditText{preferredSize:[70,-1]},\
+                        },\
+                        groupTwo: Group{orientation:'row',alignment:['fill','center'],alignChildren:['fill','fill'],minimumSize:[50,20],\
+                            addFX: Button{text:'Add FX'},\
+                        },\
+                    }";
+            
+            myPanel.grp = myPanel.add(res);
+            ex1 = myPanel.grp.groupOne;
+            ex2 = myPanel.grp.groupTwo;
+            
+            //Defaults
+            ex1.exr.value = true; //box is checked
+            ex1.gamma.value = true; //boxed is checked
+            ex1.gamEdit.text = "2.2"; //default gamma correction
+            
+            //Setup panel sizing
+//~             myPanel.layout.layout(true);
+//~             myPanel.grp.minimumSize = myPanel.grp.size;
+            
+            //Resizeability
+//~             myPanel.layout.resize();
+//~             myPanel.onResizing = myPanel.onResize = function(){this.layout.resize()};
+
+            return myPanel;
+        }
     
-var groupOne = myWin.add("group",undefined, "Group1");
-    groupOne.add("button", undefined, "Extract EXR");
+        var myScriptPal = myScript_buildUI (thisObj);
+        
+        if((myScriptPal != null) && (myScriptPal instanceof Window)){
+            myScriptPal.center;
+            myScriptPal.show();
+            }
     
- myWin.show();
+}
+            
+myScript(this);
+}
+
+ex2.addFX.onClick = function() {
+    // create an undo group
+    app.beginUndoGroup("Extraction");
+
+    var curItem = app.project.activeItem;
+    var selectedLayers = curItem.selectedLayers;
+
+    for (i=0; i<selectedLayers.length; i++){
+        var curLayer = curItem.selectedLayers[i];
+        
+        // Add checked off effects to the selected layers.
+            if (ex1.exr.value == true) {
+                var exp = curLayer.Effects.addProperty("EXtractoR");
+            }
+        
+            if (ex1.gamma.value == true) {
+                var gammaAmount = parseFloat(ex1.gamEdit.text);
+                var gamma = curLayer.Effects.addProperty("ADBE Exposure2");
+                gamma.property("ADBE Exposure2-0005").setValue(gammaAmount);
+            }
+
+    }
+
+    // close the undo group
+    app.endUndoGroup();
+}
